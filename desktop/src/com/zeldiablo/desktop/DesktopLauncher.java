f2892b07d85c74eca28e0e86a247030e0409d626
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-10-19 22:20:24 +0200

    Configuration de la taille de la fenetre

--- desktop/src/com/zeldiablo/desktop/DesktopLauncher.java
+++ desktop/src/com/zeldiablo/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.zeldiablo.Zeldiablo;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 720;
+		config.a = 8;
 		new LwjglApplication(new Zeldiablo(), config);
 	}
 }
