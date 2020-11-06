e29908f799f915a4f8a494518b3ff49d87696e1e
Hardcare <32192040+Hardcare@users.noreply.github.com>
2020-11-06 21:23:05 +0100

    Mise en place attaque speed /!\ Manque équilibrage et suppression des anciennes attaques

--- core/src/com/zeldiablo/models/Hammer.java
+++ core/src/com/zeldiablo/models/Hammer.java
@@ -3,6 +3,7 @@ package com.zeldiablo.models;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import com.badlogic.gdx.utils.Timer;
 
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
@@ -13,35 +14,50 @@ public class Hammer extends Cac {
     private int reach;
     private int damage;
     private Body hitbox;
+    private boolean canAtk;
+    private Timer timer;
+    private Timer.Task timertask;
 
     public Hammer(){
         this.damage = 1;
         this.atkspeed = 1;
         this.width = 38;
         this.reach = 45;
+        this.canAtk = true;
+        timer = new Timer();
+        timertask = new Timer.Task() {
+            @Override
+            public void run() {
+                canAtk = true;
+            }
+        };
     }
 
     @Override
     public void attack(float x, float y, float angle, World world){
-        BodyDef bd = new BodyDef();
-        bd.type = BodyDef.BodyType.DynamicBody;
-        bd.position.set(x + (float)cos(angle)*43, y + (float)sin(angle)*43);
-        this.hitbox = world.createBody(bd);
-        FixtureDef fixture = new FixtureDef();
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(reach / 2f, width / 2f, new Vector2(0, 0), angle);
-        fixture.shape = shape;
-        fixture.density = 1f;
-        fixture.restitution = 0f;
-        fixture.friction = 0f;
-        hitbox.createFixture(fixture);
-        shape.dispose();
+        if (canAtk) {
+            BodyDef bd = new BodyDef();
+            bd.type = BodyDef.BodyType.DynamicBody;
+            bd.position.set(x + (float) cos(angle) * 42, y + (float) sin(angle) * 42);
+            this.hitbox = world.createBody(bd);
+            FixtureDef fixture = new FixtureDef();
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(reach / 2f, width / 2f, new Vector2(0, 0), angle);
+            fixture.shape = shape;
+            fixture.density = 1f;
+            fixture.restitution = 0f;
+            fixture.friction = 0f;
+            hitbox.createFixture(fixture);
+            shape.dispose();
+            canAtk = false;
+            timer.scheduleTask(timertask, 2);
+        }
     }
 
     @Override
     public void draw(SpriteBatch sb) {
         sb.begin();
-        // TODO : Ajouter texture Sword
+        // TODO : Ajouter texture Hammer
         sb.end();
     }
 
