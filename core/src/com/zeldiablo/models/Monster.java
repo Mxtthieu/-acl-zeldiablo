91f36a5af3148281df9359e2e1a6ee7f3dedac5d
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-11-07 21:21:16 +0100

    Ajout du PathFinding au monstre

--- core/src/com/zeldiablo/models/Monster.java
+++ core/src/com/zeldiablo/models/Monster.java
@@ -1,8 +1,10 @@
 package com.zeldiablo.models;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.physics.box2d.Shape;
+import com.zeldiablo.controllers.ai.PathFinding;
 
 import java.awt.*;
 import java.util.List;
@@ -12,7 +14,7 @@ public abstract class Monster implements Entity {
     private Entity target;
     private Body body;
 
-    private List<Point> path;
+    private PathFinding finding;
 
     public Monster(World world, float x, float y, Entity tar) {
         this.target = tar;
@@ -33,9 +35,41 @@ public abstract class Monster implements Entity {
         body.setUserData(this);
         body.createFixture(fixture);
         shape.dispose();
+
+        boolean[][] grid = new boolean[GameWorld.HEIGHT][GameWorld.WIDTH];
+        for (int j = 0; j < GameWorld.HEIGHT; j++)
+            for (int i = 0; i < GameWorld.WIDTH; i++)
+                grid[j][i] = true;
+
+        this.finding = new PathFinding(grid, this.getX(), this.getY(), this.target.getX(), this.target.getY());
+        System.out.println(this.finding);
     }
 
-    public void calculatePathToTarget() {
+    /**
+     * Methode seravnt a récupérer la position de l'entité
+     * @return Vector2 de la position
+     */
+    public Vector2 getPosition() {
+        return this.body.getPosition();
+    }
 
+    /**
+     * Méthode servant a récupérer la position en X de l'entité
+     *
+     * @return int coordonée X
+     */
+    @Override
+    public int getX() {
+        return (int) this.getPosition().x;
+    }
+
+    /**
+     * Méthode servant a récupérer la position en Y de l'entité
+     *
+     * @return int coordonée Y
+     */
+    @Override
+    public int getY() {
+        return (int) this.getPosition().y;
     }
 }
