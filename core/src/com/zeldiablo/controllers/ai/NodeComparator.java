5fad85e389b67b337b72855f8049818e374dcf2b
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-11-07 18:29:41 +0100

    Ajout d'un comparateur de noeud

---
+++ core/src/com/zeldiablo/controllers/ai/NodeComparator.java
@@ -0,0 +1,24 @@
+package com.zeldiablo.controllers.ai;
+
+import java.util.Comparator;
+
+public class NodeComparator implements Comparator<Node> {
+    /**
+     * Compare un noeud n au noeud courant.
+     * @param n1 Premier noeud
+     * @param n2 Deuxième noeud
+     * @return  1 si l'heuristique de n1 est inférieur à n2
+     *          0 si les deux heuristiques sont égaux
+     *          -1 si il est supérieur
+     */
+    @Override
+    public int compare(Node n1, Node n2) {
+        if (n1.heuristic < n2.heuristic)
+            return 1;
+        else if (n1.heuristic == n2.heuristic)
+            return 0;
+        else
+            return -1;
+    }
+
+}
