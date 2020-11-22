2736d3b01791fdafd3793cb2f329379dc4edfcfa
Pedro Sousa <pedro13sousa@hotmail.fr>
2020-11-22 19:40:57 +0100

    Définition du menu principal comme ecran de démarage

--- core/src/com/zeldiablo/Zeldiablo.java
+++ core/src/com/zeldiablo/Zeldiablo.java
@@ -1,21 +1,13 @@
 package com.zeldiablo;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.zeldiablo.views.GameScreen;
+import com.zeldiablo.views.MainMenu;
 
 public class Zeldiablo extends Game {
 
-	private GameScreen gameScreen;
-
 	@Override
 	public void create() {
-		this.gameScreen = new GameScreen();
-		setScreen(this.gameScreen);
+		setScreen(new MainMenu());
 	}
 }
 
